8da388b73cce4a0f074156506cce174c7e574de2
sujan khatri <sujansince2059@gmail.com>
2024-01-17 10:42:48 +0545

    built the function for password generation

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
+  const [isnumallowed, setIsnumallowed] = useState(false);
+  const [ischarallowed, setIscharallowed] = useState(false);
+
+  const [Password, setPassword] = useState("");
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (isnumallowed) {
+      string += "0123456789";
+    }
+    if (ischarallowed) {
+      string += "!@#$%^&*()-_=+[]{}|;:,.<>?/";
+    }
+
+    for (let index = 1; index <= length; index++) {
+      let char = Math.floor(Math.random() * string.length + 1);
+      pass += string.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, ischarallowed, isnumallowed, setPassword]);
+  generatePassword();
   return (
     <>
-      <h1 className="text-4xl text-center">Password Generator</h1>
+      <h1 className="text-4xl text-center">Password Generator + {Password}</h1>
+      <button onClick={generatePassword}>xdddd</button>
+      <h2></h2>
     </>
   );
 }
