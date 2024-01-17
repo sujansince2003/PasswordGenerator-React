3421ac1cb4b083b7e8527fd0cab257f5095871ef
sujan khatri <sujansince2059@gmail.com>
2024-01-17 09:40:51 +0545

    basic project setup plus added tailwind css

---
+++ passwordGenerator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  return (
+    <>
+      <h1 className="text-4xl text-center">Password Generator</h1>
+    </>
+  );
+}
+
+export default App;
