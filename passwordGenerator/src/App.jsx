67315565a801b9f882d3b8f6e3c2c07e825ef306
sujan khatri <sujansince2059@gmail.com>
2024-01-17 16:58:31 +0545

    workin in UI

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -24,12 +24,29 @@ function App() {
     }
     setPassword(pass);
   }, [length, ischarallowed, isnumallowed, setPassword]);
-  generatePassword();
+  // generatePassword();
   return (
     <>
-      <h1 className="text-4xl text-center">Password Generator + {Password}</h1>
-      <button onClick={generatePassword}>xdddd</button>
-      <h2></h2>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 ">
+        <h1 className="text-white text-center ">Password generator</h1>
+        <div className="flex flex-col rounded-lg overflow-hidden mb-4">
+          <input
+            className="outline-none w-full py-1 px-3 rounded-lg mb-4"
+            type="text"
+            value={Password}
+            readOnly
+          />
+          <div className="flex flex-col gap-4 pl-12 py-10 items-start bg-gray-500">
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+            <input type="checkbox" />
+          </div>
+        </div>
+
+        <button onClick={generatePassword}>generate</button>
+      </div>
     </>
   );
 }
